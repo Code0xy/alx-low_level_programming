67771e1316dcce2a1b471245707515c24cbf8d09
Code0xy <lindaoliv15@gmail.com>
2022-06-30 18:30:14 -0700

    Another printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the size of various types
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar,but the outcome is a piece of art\n");
+return (0);
+} 
