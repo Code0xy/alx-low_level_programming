da07571879d84b0784d63352b92c5c69c7074ffa
Code0xy <lindaoliv15@gmail.com>
2022-06-30 19:18:37 -0700

    C is file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,4 @@
 #include <stdio.h>
 /**
- * main - prints the size of various types
- * 
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-printf("with proper grammar,but the outcome is a piece of art\n");
-return (0);
-} 
+ * main - prints the size of various types 
+ h Return: 0 if existed properly, non zero otherwise
