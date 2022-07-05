74408795142689cda4dfdf7662fcd7ff47410a54
Code0xy <lindaoliv15@gmail.com>
2022-07-04 18:20:32 -0700

    The last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
+
+/**
  *main -> assign a random number to the variable n each time it is executed and prints out
  *based a condition
- *Return : always 0
+ *Return: Always 0
 */   
-int main - assign a random number to the variable n each time it is executed
+int main(void)
 { 
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
 *main -  assign a random number to the variable n each time it is executed
-*in order to print the last digit of the number stored in the variable n
+*print the last digit of the number stored in the variable n
 *Return: Always 0
 */
 int main(void)
