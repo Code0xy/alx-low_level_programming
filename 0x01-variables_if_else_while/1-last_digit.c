84cd4b249ad524555400dc18d0c421a200d87c1a
Code0xy <lindaoliv15@gmail.com>
2022-07-05 07:05:17 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,17 +13,18 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+printf("Last digit of %d is ", n);
 if (n > 5)
 {
-printf("Last digit of 98 is 8 and is greater than 5");
+printf("greater than 5");
 }
 if (n == 0)
 {
-printf("Last digit of 980 is 0 and is 0");
+printf("0");
 }
 if (n < 6 && n != 0)
 {
-printf("Last digit of -98 is -8 and is less than 6 and  not 0");
+printf("less than 6 not 0");
 }
 printf("\n");
 return (0);
