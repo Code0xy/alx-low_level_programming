4ac51066bcff349dc6a36363b6fd2874bfe9e114
Code0xy <lindaoliv15@gmail.com>
2022-07-05 08:37:15 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,8 +12,8 @@ for (ch = 97; ch <= 122; ch++)
 {
 putchar(ch);
 }
-putchar(10);
 
+putchar('\n');
 return (0);
 }
 
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 
 /**
- * main -  entry point
- *
+ * main -  a program that prints the alphabet in lowercase,
+ *  followed by a new line.
  *  Return: Always 0
  */
 int main(void)
 {
-char a;
-for (a = 'a'; a <= 'z'; a++)
-putchar(a);
+int ch;
+for (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
 
-for (a = 'A'; ch <= 'Z'; a++)
-putchar(a);
-putchar(‘\n’);
+putchar('\n');
+for (ch = 65; ch <= 90; ch++)
+{
+putchar(ch);
+}
+
+putchar('\n');
+
 return (0);
 }
+
 
