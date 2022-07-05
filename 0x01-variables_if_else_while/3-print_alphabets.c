00188225c255bc4d918a7309585d4fa2d18de8b1
Code0xy <lindaoliv15@gmail.com>
2022-07-05 08:15:13 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,20 +5,20 @@
  * then uppercase
  *  followed by a new line.
  *  Return: Always 0
- */
+ **/
 int main(void)
 {
 int ch;
-for (ch = A; ch <= Z; ch++)
-{ putchar(ch);
-}
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+
 putchar(‘\n’);
  /* print lowercase letters */
 
-for (ch = a; ch <= z; ch++)
-{
+for (ch = 'a'; ch <= 'z'; ch++)
+
 putchar(ch);
-}
+
 putchar(‘\n’);
 return (0);
 }
