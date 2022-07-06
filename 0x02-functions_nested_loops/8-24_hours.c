28a12dc9dca4106ee784db56794e9dedc62a30fe
Code0xy <lindaoliv15@gmail.com>
2022-07-05 18:50:12 -0700

    Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,11 +5,13 @@
 void jack_bauer(void)
 {
 int i, j;
+
 for (i = 0; i < 24; i++)
 {
 for (j = 0; j < 60; j++)
 {
 if (i < 10)
+{
 _putchar('0');
 _putchar(i + '0');
 }
@@ -25,9 +27,11 @@ _putchar('0');
 _putchar(j + '0');
 }
 else if (j > 10)
+{
 _putchar(':');
 _putchar((j / 10) + '0');
 _putchar((j % 10) + '0');
 }
 }
-
+}
+}
