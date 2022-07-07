b8b4ad03ac6996d82c16985037c9c074d2e021b9
Code0xy <lindaoliv15@gmail.com>
2022-07-07 15:36:28 -0700

    Love numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+int a, b;
+
+for (a = 0; x < 10; a++)
+{
+for (b = 0; b < 15; b++)
+{
+_putchar(b + '0');
+}
+_putchar('\n');
+}
