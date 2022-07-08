5d82e771203b1f0ba5a5b0c5a8d1b6f4e0c3a695
Code0xy <lindaoliv15@gmail.com>
2022-07-07 18:34:13 -0700

    Parallel universe

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,23 +6,12 @@
  */
 void print_diagonal(int n)
 {
-int x, i;
+int x;
 
-if (n <= 0)
-_putchar('\n');
-else
+for (x = 0; x <= n; x++)
 {
-
-for  (x = 0; x < n; x++)
-{
-
-for (i = 0; i < x; i++)
-{
-
 _putchar(92);
 _putchar('\n');
 _putchar(' ');
-}
-}
 }
 }
