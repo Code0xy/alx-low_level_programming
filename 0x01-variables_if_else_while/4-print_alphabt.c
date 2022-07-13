9404cae9f1f2b2c0d62a1d920d23baed63804eeb
Code0xy <lindaoliv15@gmail.com>
2022-07-13 13:11:54 -0700

    Function c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,16 @@
 int main(void)
 {
 char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-{
-if (letter == 'q' || letter == 'e')
-{
-continue;
-}
-putchar(letter);
-return (0);
-}}
 
+for (letter = 'a'; letter <= 'z'; letter++)
+
+{
+
+	if (letter != 'e' && letter != 'q')
+		putchar(letter);
+}
+
+putchar('\n');
+return (0);
+
+}
