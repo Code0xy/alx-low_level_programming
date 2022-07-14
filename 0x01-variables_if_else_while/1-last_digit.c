e35e9902c7ed509c6c2b295d5344112eccf89ca7
Code0xy <lindaoliv15@gmail.com>
2022-07-13 18:37:51 -0700

    Greater digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ last = n % 10;
 if (last >= 6)
 	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 
-else if (Last < 6 && last != 0)
+else if (last < 6 && last != 0)
 	printf("Last digit of %d is %d and is less than 6 and not 0\n" n, n % 10);
 
 else
