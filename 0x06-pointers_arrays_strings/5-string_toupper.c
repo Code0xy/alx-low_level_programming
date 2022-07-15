779e16f3c3519cd9a5c3cab76f4a351375583122
Code0xy <lindaoliv15@gmail.com>
2022-07-14 18:46:18 -0700

    Separators

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * string_toupper - converts lowercase letters of a string to uppercase
+ * @ch: pointer to char
+ * Return: Always 0
+ */
+
+char *string_toupper(char *)
+
+{
+	int k = 0;
+
+	while (*(ch + k) != '\0')
+
+	{
+		if (*(ch + k) >= 97 && *(ch + k) <= 122)
+			*(ch + k) = *(ch + k) - ' ';
+
+		k++;
+
+	}
+return (ch);
+
+}
