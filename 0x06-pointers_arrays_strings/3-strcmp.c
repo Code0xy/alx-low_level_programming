0897ca5f910c9aa8a5341827fa76a99cba73ae01
Code0xy <lindaoliv15@gmail.com>
2022-07-14 17:42:22 -0700

    Two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: the destination string
+ * @s2: the string to be copied
+ * Return: Always 0
+ */
+int _strcmp(char *s1, char *s2)
+
+{
+	int i = 0
+
+		while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+			i++;
+
+	if (*(s2 + i))
+		return (*(s1 + i) - *(s2 + i));
+
+	else
+		return (0);
+}
