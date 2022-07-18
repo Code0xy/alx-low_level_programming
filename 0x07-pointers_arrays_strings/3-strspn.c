75e9f211757650a2c371668fa34948100789823d
Code0xy <lindaoliv15@gmail.com>
2022-07-18 16:36:54 -0700

    Length of prefix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strspn -  function that returns the number of bytes
+ * @s: string
+ * @accept: string
+ * Return: integer
+ */
+unsigned int _strspn(char *s, char *accept)
+
+{
+	int p;
+
+	p = strspn(s, accept);
+
+	return (p);
+
+}
