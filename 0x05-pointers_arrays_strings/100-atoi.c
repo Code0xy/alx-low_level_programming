4985a3ff58042257a6035afe6208eeddcd9aaf25
Code0xy <lindaoliv15@gmail.com>
2022-07-24 13:03:24 -0700

    Atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - function that convert a string to an integer
+ *@s: infinite number
+ * Return: Always 0
+ */
+int _atoi(char *s)
+
+{
+	int xy;
+
+	xy = _atoi("98");
+	printf("%d\n", xy);
+
+	return (0);
+
+}
