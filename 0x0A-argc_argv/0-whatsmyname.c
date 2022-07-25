3c1f8b6bd21cb09b090a84c4982c36fbf14c13fd
Code0xy <lindaoliv15@gmail.com>
2022-07-25 05:24:43 -0700

    My name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,14 +3,21 @@
 
 /**
  * main - a program that prints its name
- * @argv - the program
+ * @argv:count of the arguments
+ * @argc:array of poniters to the strings
  *  Return: Always 0
  */
 
-int main(int argc char *argv)
+int main(int argc, char *argv)
 {
-	printf("My name is Linda", \n);
-	getch();
+int y;
+
+for (y = 0; y < argc; y++)
+
+{
+	printf("%s\n", argv[i]);
+
+}
 
 	return (0);
 
