379311521ddf7452df3e7537e6f6c00c17e44829
Code0xy <lindaoliv15@gmail.com>
2022-08-03 17:06:24 -0700

    Print a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name
  * @name: name of the person
- * @f: a function that prints a name
+ * @f: function pointer
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
