4cb3b793ac0ccf2bc4e2115b26898bce41c86b85
Code0xy <lindaoliv15@gmail.com>
2022-08-21 13:20:05 -0700

    Memory

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -17,11 +17,13 @@ if (argc != 3)
 printf("Error\n");
 exit(98);
 }
+
 else
+
 {
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-result = num1 * num2;
+result = num1  *num2;
 printf("%d\n", result);
 return (0);
 }
